7dd81e8d946e1bbe0272ffed29a2214113d0dd3f
Modupe Ade-Onojobi <modupeadeonojobi@gmail.com>
2023-06-22 15:01:59 +0100

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'properties/index'
+      get 'properties/show'
+      get 'properties/create'
+      get 'properties/update'
+      get 'properties/destroy'
+    end
+  end
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
